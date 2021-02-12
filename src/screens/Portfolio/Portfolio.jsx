922843b3acf799a657526c2a151e822033ac2f88
Connor McKenna <connormckenna2654@gmail.com>
2021-02-12 17:22:00 -0500

    updated styling and airtable info

--- src/screens/Portfolio/Portfolio.jsx
+++ src/screens/Portfolio/Portfolio.jsx
@@ -8,11 +8,11 @@ function Portfolio(props) {
       <>
         <div className="portfolio-container">
         <div className="projects-container">
-            <p>Here's a sample of projects I've worked on!</p>
+            <p>Here's a sample of my work!</p>
             {props.listProjects &&
-              <div key={i++} className="projects">
+            <div className="projects">
               {props.listProjects.map((item) =>
-                <Link className="project-link" to={`/portfolio/${item.fields.name}`}>
+                <Link key={item.fields.id} className="project-link" to={`/portfolio/${item.fields.name}`}>
                   <div className='project-img' style={{ backgroundImage: `url(${item.fields.thumbnail}`}}>
                     <div className='project-info-container'>
                       <p className='project-info'>{item.fields.name}</p>
